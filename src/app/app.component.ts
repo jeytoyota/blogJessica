5fee8295096d12e66ccaf6f6887e35a7410472a3
jeytoyota <jeytoyota@gmail.com>
2021-06-30 16:05:49 -0300

    página-inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 
 @Component({
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blogJessica';
-  
+  constructor(
+    public auth: AuthService
+  ){}
 }
